8148b036e9a92d1a39a187de205045249b299507
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-04-04 13:41:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,111 @@
-let hora = document.querySelector("#hora")
-let minuto = document.querySelector("#minuto")
-let segundo = document.querySelector("#segundo")
-let x = 0
-let comeca = false
+let mostraHora = document.querySelector("#hora");
+let mostraMinuto = document.querySelector("#minuto");
+let mostraSegundo = document.querySelector("#segundo");
+let mostraMilisegundo = document.querySelector("#milisegundo");
+let hora = (minuto = segundo = milisegundo = 0);
+let comeca = false;
+let botao = document.querySelector("#start");
 
-// começa contagem regressiva
-function start(){
+// liga e desliga o cronometro
+const troca = () => {
+  if (comeca == true) {
+    comeca = false;
+    botao.innerHTML = "Start";
+  } else {
+    comeca = true;
+    botao.innerHTML = "Pause";
+  }
+};
 
-    if(comeca == true){
-        document.querySelector("audio").pause()
+// se o cronometro tiver começado segundo e minuto é incrementado
+const start = () => {
+  // incrementa segundos, minutos e hora
+  if (comeca == true) {
+    milisegundo += 1;
 
-        if(minuto.value == ""){
-            minuto.value = 0
-        }
-        if(hora.value == ""){
-            hora.value = 0
-        }
-        if(segundo.value == ""){
-            segundo.value = 0
-        }
+    if (milisegundo == 60) {
+      milisegundo = 0;
+      segundo += 1;
 
-        if(minuto.value == 0 && hora.value != 0){
-            minuto.value = 60
-            hora.value -= 1
+      if (segundo == 60) {
+        segundo = 0;
+        minuto += 1;
 
+        if (minuto == 60) {
+          minuto = 0;
+          hora += 1;
         }
-        if(segundo.value == 0 && minuto.value != 0){
-            segundo.value = 60
-            minuto.value -= 1
-        
-        }
-        
-        segundo.value -= 1
-
-        if(segundo.value == 0 && minuto.value == 0 && hora.value == 0){
-            document.querySelector("audio").play()
-            troca()
-        }
+      }
     }
-}
 
-// liga e desliga o contador
-function troca(){
-    if(comeca == true){
-        comeca = false
-        document.querySelector("button").innerHTML= 'Start'
-    }else{
-        
-        if(hora.value == "" && minuto.value == "" && segundo.value == ""){
-            alert("insira algum valor numerico")
-        }else if(hora.value == 0 && minuto.value == 0 && segundo.value == 0){
-            alert("insira algum valor numerico diferente de zero")
-        }else if(isNaN(hora.value) || isNaN(minuto.value) || isNaN(segundo.value)){
-            alert("insira somente valores numericos")
-        }else{
-            comeca = true
-            document.querySelector("button").innerHTML= 'Pause'
-        }
+    // verifica quantidade de digitos para permanecer dois digitos cara contador
+    if (milisegundo.toString().length > 1) {
+      mostraMilisegundo.innerHTML = milisegundo;
+    } else {
+      mostraMilisegundo.innerHTML = 0 + milisegundo.toString();
     }
-}
+
+    if (segundo.toString().length > 1) {
+      mostraSegundo.innerHTML = segundo;
+    } else {
+      mostraSegundo.innerHTML = 0 + segundo.toString();
+    }
+
+    if (minuto.toString().length > 1) {
+      mostraMinuto.innerHTML = minuto;
+    } else {
+      mostraMinuto.innerHTML = 0 + minuto.toString();
+    }
+
+    if (hora.toString().length > 1) {
+      mostraHora.innerHTML = hora;
+    } else {
+      mostraHora.innerHTML = 0 + hora.toString();
+    }
+  }
+};
+
+// reseta o cronometro
+const reset = () => {
+  milisegundo = 0;
+  segundo = 0;
+  minuto = 0;
+  hora = 0;
+
+  if (milisegundo.toString().length > 1) {
+    mostraMilisegundo.innerHTML = milisegundo;
+  } else {
+    mostraMilisegundo.innerHTML = 0 + milisegundo.toString();
+  }
+  
+  if (segundo.toString().length > 1) {
+    mostraSegundo.innerHTML = segundo;
+  } else {
+    mostraSegundo.innerHTML = 0 + segundo.toString();
+  }
+
+  if (minuto.toString().length > 1) {
+    mostraMinuto.innerHTML = minuto;
+  } else {
+    mostraMinuto.innerHTML = 0 + minuto.toString();
+  }
+
+  if (hora.toString().length > 1) {
+    mostraHora.innerHTML = hora;
+  } else {
+    mostraHora.innerHTML = 0 + hora.toString();
+  }
+
+  comeca = false;
+  botao.innerHTML = "Start";
+};
 
 // aciona funçao pela tecla precionada
-const enter = (event) =>{
+const enter = (event) => {
+  if (event.which == 13 || event.which == 32) {
+    troca();
+  }
+};
 
-    if(event.which == 13 || event.which == 32){
-        troca()
-    }
-}
-
-setInterval(start, 1000)
+// chama funçao start a cada segundo
+setInterval(start, 10);
